f5d2edc669e83326b563cbb6cf16190462fed2fd
JMAgstn <119966783+JMAgstn@users.noreply.github.com>
2023-03-10 10:18:41 +0800

    initial commit

---
+++ src/components/AddTask.js
@@ -0,0 +1,45 @@
+export const AddTask = ({ tasklist, setTasklist, task, setTask }) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (task.id) {
+      const date = new Date();
+      const updatedTask = tasklist.map((todo) =>
+        todo.id === task.id
+          ? {
+              id: task.id,
+              name: e.target.task.value,
+              time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`,
+            }
+          : todo
+      );
+      setTasklist(updatedTask);
+      setTask({});
+    } else {
+      const date = new Date();
+      const newTask = {
+        id: date.getTime(),
+        name: e.target.task.value,
+        time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`,
+      };
+      setTasklist(tasklist.concat(newTask));
+      setTask({});
+    }
+  };
+  return (
+    <section className="addTask">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="task"
+          autoComplete="off"
+          placeholder="add task"
+          maxLength="25"
+          value={task.name || ""}
+          onChange={(e) => setTask({ ...task, name: e.target.value })}
+        />
+        <button type="submit">{task.id ? "Edit" : "Add"}</button>
+      </form>
+    </section>
+  );
+};
